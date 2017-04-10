165e4ebd29131cfec6edaae88d8c9059dd32d9d6
Jack <jmengledow@indy.rr.com>
2017-04-10 12:50:10 -0400

    started scramble generator

--- Timer/timer.js
+++ Timer/timer.js
@@ -2,21 +2,29 @@ var inspect = 15;
 var space = 1;
 var doops;
 var interval;
-var startTime = Date.now();
+var random;
+var randomLength;
+var scrambleArray = ["R", "R2", "R'", "L", "L2", "L'", "U", "U2", "U'", "B", "B2", "B'", "B", "D2", "D'", "F", "F2", "F'"];
 
 $(document).on("keypress", function(e){
 	if (e.keyCode == 32){
-		if(space % 2 == 0){
+		if(space == 2){
 			e.preventDefault();
 			clearInterval(doops);
+			var startTime = Date.now();
 			interval = setInterval(function() {
 			var elapsedTime = Date.now() - startTime;
 			document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
 			}, 100);
+			space = 3;
+		}
+		else if(space == 3){
+			clearInterval(interval);
 			space++;
+			generateScramble();
 		}
 		else{
-			space++;
+			space = 2;
 			clearInterval(interval);
 			e.preventDefault();
 			inspect = 15;
@@ -32,4 +40,14 @@ var decrement = function(){
 var increment = function(){
 	timeStart++;
 	$("p").html(timeStart);
+}
+var getRandom = function(){
+	random = Math.floor(Math.random() * 17);
+}
+var generateScramble(){
+	getRandom();
+	randomLength = Math.floor(Math.random() * 20);
+	if (randomLength < 15){
+		randomLength = 15;
+	}	
 }
