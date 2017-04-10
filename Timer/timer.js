517f94b8214a99e55db49b7d20406e4122e5837b
Jack <jmengledow@indy.rr.com>
2017-04-10 12:28:30 -0400

    files

--- Timer/timer.js
+++ Timer/timer.js
@@ -1,11 +1,35 @@
-$(document).ready(function(){
-   $(document).keypress(function (e) {
-      if (e.keyCode == 32) {
-         e.preventDefault()
-         console.log('Space pressed');
-         }
-      else{
-         console.log("fail");
-      }
-   });
+var inspect = 15;
+var space = 1;
+var doops;
+var interval;
+var startTime = Date.now();
+
+$(document).on("keypress", function(e){
+	if (e.keyCode == 32){
+		if(space % 2 == 0){
+			e.preventDefault();
+			clearInterval(doops);
+			interval = setInterval(function() {
+			var elapsedTime = Date.now() - startTime;
+			document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(3);
+			}, 100);
+			space++;
+		}
+		else{
+			space++;
+			clearInterval(interval);
+			e.preventDefault();
+			inspect = 15;
+			$("p").html(inspect);
+			doops = setInterval(decrement, 1000);
+		}
+	}
 });
+var decrement = function(){
+	inspect--;
+	$("p").html(inspect);
+}
+var increment = function(){
+	timeStart++;
+	$("p").html(timeStart);
+}
---
+++ vigenere/cracker.js
@@ -0,0 +1,71 @@
+var t = 0;
+var o = 0;
+var message = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var object = {
+	a: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+	b: ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a"],
+	c: ["c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b"],
+	d: ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"],
+	e: ["e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d"],
+	f: ["f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e"],
+	g: ["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f"],
+	h: ["h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g"],
+	i: ["i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h"],
+	j: ["j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i"],
+	k: ["k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+	l: ["l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"],
+	m: ["m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"],
+	n: ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"],
+	o: ["o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"],
+	p: ["p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"],
+	q: ["q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"],
+	r: ["r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"],
+	s: ["s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"],
+	t: ["t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"],
+	u: ["u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"],
+	v: ["v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u"],
+	w: ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"],
+	x: ["x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w"],
+	y: ["y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x"],
+	z: ["z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z"],
+}
+var encrypt = function(){
+	var index;
+	var answer = [];
+	var key = document.getElementById("key").value;
+	var something = key.split("");
+	var encode = document.getElementById("encode").value;
+	var encoded = encode.split("");
+	while(t <= encoded.length){
+		index = message.indexOf(encoded[t]);
+		var place = something[o];
+		var letter = object[place][index];
+		answer.push(letter);
+		t++;
+		o++;
+		if(o === something.length){
+			o = 0;
+		}
+	}
+	document.getElementById("real").innerHTML = answer.join("");
+}
+var makesense = function(){
+	var key = document.getElementById('key').value;
+	var something = key.split("");
+	var decode = document.getElementById("decode").value;
+	var decoded = decode.split("");
+	var decryptedMessge = [];
+	while(t <= decoded.length){
+		var place2 = something[o];
+		var row = object[place2];
+		var index = row.indexOf(decoded[t]);
+		var letter = message[index];
+		decryptedMessge.push(letter);
+		t++;
+		o++;
+		if(o === something.length){
+			o = 0;
+		}
+	}
+	document.getElementById("fake").innerHTML = decryptedMessge.join("");
+}
